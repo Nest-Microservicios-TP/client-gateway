c8fd68b913532e3daac62d0cb06ae20d9870bf3a
Mateo Rodriguez <mateor861@gmail.com>
2025-10-07 18:04:34 -0300

    Feat: Orders logic

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,41 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(@Inject(ORDER_SERVICE) private readonly OrderClient: ClientProxy) { }
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly orderClient: ClientProxy,
+  ) { }
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.OrderClient.send({cmd: 'createOrder'}, {}).pipe(
-      catchError(err => { throw new RpcException(err) })
+    return this.orderClient.send({ cmd: 'createOrder' }, createOrderDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
     );
   }
 
   @Get()
   findAll() {
-    return this.OrderClient.send({cmd: 'findAllOrders'}, {}).pipe(
-      catchError(err => { throw new RpcException(err) })
+    return this.orderClient.send({ cmd: 'findAllOrders' }, {}).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
     );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.OrderClient.send({cmd: 'findOneOrder'}, { id }).pipe(
-      catchError(err => { throw new RpcException(err) })
+  findOne(@Param('id') id: string) {
+    return this.orderClient.send({ cmd: 'findOneOrder' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
     );
   }
 
-  @Patch(':id')
-  changeOrderStatus(@Param('id') id: number) {
-    return this.OrderClient.send({cmd: 'changeOrderStatus'}, { id }).pipe(
-      catchError(err => { throw new RpcException(err) })
-    );
+  @Get(':id/status')
+  changeOrderStatus(@Param('id') id: string) {
+    return this.orderClient
+      .send({ cmd: 'changeOrderStatus' }, { id })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
   }
-
-
 }
